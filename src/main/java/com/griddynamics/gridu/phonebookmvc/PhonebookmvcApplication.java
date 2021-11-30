f77f99a74082ba5bf5f7c4ad3120ee072dbf4f03
Aludey <vladnikitin486@yandex.ru>
2021-11-30 17:53:19 +0300

    Initial commit

---
+++ src/main/java/com/griddynamics/gridu/phonebookmvc/PhonebookmvcApplication.java
@@ -0,0 +1,13 @@
+package com.griddynamics.gridu.phonebookmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/griddynamics/gridu/phonebookmvc/PhonebookmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamics.gridu.phonebookmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
